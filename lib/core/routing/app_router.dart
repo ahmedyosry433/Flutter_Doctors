40f785d373dec7397b94c89a4c1f8c02177a777b
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2023-12-21 18:10:11 +0200

    Update app_router.dart

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:doctor_appointment/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/onboarding/onboradingScreen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    // this arguments to be pass in any screen like this ( arguments as className)
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case Routes.onboarderScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text(' No Route Defined For ${settings.name}'),
+                  ),
+                ));
+    }
+  }
+}
