23ca66f46b6eab4adab6eef095a59da7a5bb2aa4
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2024-01-20 11:53:22 +0200

    save token in shared preferences
     - added shared preferences helper file

--- lib/features/login/logic/cubit/login_cubit.dart
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment/core/helper/shared_preferences_helper.dart';
 import 'package:doctor_appointment/features/login/data/models/login_request_body.dart';
 import 'package:doctor_appointment/features/login/data/repos/login_repo.dart';
 import 'package:flutter/widgets.dart';
@@ -6,20 +7,24 @@ import 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
-
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
 
-  void emitLoginStates( ) async {
+  void emitLoginStates() async {
     emit(const LoginState.loading());
 
-    final response = await _loginRepo.login(LoginRequestBody(email: emailController.text, password: passwordController.text));
+    final response = await _loginRepo.login(LoginRequestBody(
+        email: emailController.text, password: passwordController.text));
 
     response.when(success: (loginResponse) {
-      emit(LoginState.success(loginResponse));
+      SharedPreferencesHelper.setValueForKey(
+          'token', loginResponse.userData!.token);
+      emit(
+        LoginState.success(loginResponse),
+      );
     }, failure: (error) {
       emit(LoginState.error(error: error.apiErrorModel.message ?? ''));
     });
