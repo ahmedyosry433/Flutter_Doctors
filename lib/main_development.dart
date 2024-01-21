cc33ab972a741da56220ed0a64f76f24de320508
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2024-01-21 18:28:40 +0200

    flavor development & production
    - added launch.json for flavors config
    - changed android app name for each flavor
    - add two main files each flavor

---
+++ lib/main_development.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/dj/dependency_injection.dart';
+import 'core/routing/app_router.dart';
+import 'package:flutter/material.dart';
+
+import 'doc_app.dart';
+
+void main() async {
+  setupGetit();
+  // to fix texts begin hidden bug in flutter_screenUtil in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,14 @@
 import 'core/dj/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'doc_app.dart';
 
-void main() {
+void main() async {
   setupGetit();
+  // to fix texts begin hidden bug in flutter_screenUtil in release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
